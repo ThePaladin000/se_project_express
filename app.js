f207c1e2cb6a8d29bc8e060b0b4b92652d9d172a
Joshua Gomez <gomezjoshuam@gmail.com>
2025-09-20 17:36:50 -0400

    add code to resolve crashes

--- app.js
+++ app.js
@@ -22,6 +22,13 @@ const { requestLogger, errorLogger } = require("./middleware/logger");
 app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
+
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
+
 app.use("/", routes);
 
 app.use(errorLogger);
